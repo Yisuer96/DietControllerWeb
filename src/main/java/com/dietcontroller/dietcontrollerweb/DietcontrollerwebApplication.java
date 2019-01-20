14965852064ce62513c2e31a1d2fbb4744541f6e
Yisuer96 <31649360+Yisuer96@users.noreply.github.com>
2019-01-20 11:32:55 +0800

    init files

---
+++ src/main/java/com/dietcontroller/dietcontrollerweb/DietcontrollerwebApplication.java
@@ -0,0 +1,14 @@
+package com.dietcontroller.dietcontrollerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietcontrollerwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietcontrollerwebApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dietcontroller/dietcontrollerweb/DietcontrollerwebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dietcontroller.dietcontrollerweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DietcontrollerwebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
